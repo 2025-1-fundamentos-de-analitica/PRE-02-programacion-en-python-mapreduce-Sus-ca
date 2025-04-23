722318721212a3e2e4cc9605f86a6f8545f95a27
Sus-ca <scuenca@unal.edu.co>
2025-04-23 11:22:32 -0500

    Intento 1

--- homework/word_count.py
+++ homework/word_count.py
@@ -7,7 +7,7 @@ import glob
 import os.path
 import time
 from itertools import groupby
-
+import string
 
 #
 # Escriba la funcion que  genere n copias de los archivos de texto en la
@@ -19,6 +19,22 @@ from itertools import groupby
 #
 def copy_raw_files_to_input_folder(n):
     """Funcion copy_files"""
+
+    if not os.path.exists("files/input"):
+        os.makedirs("files/input")
+    for file in glob.glob("files/raw/*"):
+        for i in range(1, n + 1):
+            with open(file, "r", encoding="utf-8") as f:
+                with open(
+                    f"files/input/{os.path.basename(file).split('.')[0]}_{i}.txt",
+                    "w",
+                    encoding="utf-8",
+                ) as f2:
+                    f2.write(f.read())
+                    
+                    
+                    
+                  
 
 
 #
@@ -32,12 +48,15 @@ def copy_raw_files_to_input_folder(n):
 #   [
 #     ('text0'.txt', 'Analytics is the discovery, inter ...'),
 #     ('text0'.txt', 'in data. Especially valuable in ar...').
-#     ...
-#     ('text2.txt'. 'hypotheses.')
-#   ]
-#
 def load_input(input_directory):
     """Funcion load_input"""
+
+    sequence = []
+    files = glob.glob(f"{input_directory}/*")
+    with fileinput.input(files=files) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    return sequence
 
 
 #
@@ -47,6 +66,11 @@ def load_input(input_directory):
 #
 def line_preprocessing(sequence):
     """Line Preprocessing"""
+    sequence = [
+        (key, value.translate(str.maketrans("", "", string.punctuation)).lower())
+        for key, value in sequence
+    ]
+    return sequence
 
 
 #
@@ -63,6 +87,8 @@ def line_preprocessing(sequence):
 #
 def mapper(sequence):
     """Mapper"""
+    return [(word, 1) for _, value in sequence for word in value.split()]
+
 
 
 #
@@ -78,6 +104,7 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
 
 
 #
@@ -88,7 +115,10 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
-
+    result = []
+    for key, group in groupby(sequence, lambda x: x[0]):
+        result.append((key, sum(value for _, value in group)))
+    return result
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de
@@ -97,6 +127,11 @@ def reducer(sequence):
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
 
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
 
 #
 # Escriba la función save_output, la cual almacena en un archivo de texto
@@ -108,6 +143,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -116,13 +154,26 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
+    
 def run_job(input_directory, output_directory):
     """Job"""
+
+    sequence = load_input(input_directory)
+    sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
+
 
 
 if __name__ == "__main__":
